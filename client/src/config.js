32607b5f08f2ee7855b072db143c7ffff5363e11
mareligncode <yimermarelign@gmail.com>
2025-11-25 20:01:39 +0300

    Update BASE_URL for production environment

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 let BASE_URL = "https://post-it-heroku.herokuapp.com/";
 if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
-  BASE_URL = "http://localhost:4000/";
+  BASE_URL = "https://full-stack-social-media-app-three.vercel.app/";
 }
 
 export { BASE_URL };
