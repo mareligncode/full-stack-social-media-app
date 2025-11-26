7a89cd088687948e80e6ad755e60c642f8dc43c5
mareligncode <yimermarelign@gmail.com>
2025-11-26 23:00:55 +0300

    Update BASE_URL to new deployment URL

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
-let BASE_URL = "https://post-it-heroku.herokuapp.com/";
+let BASE_URL = "https://full-stack-social-media-app-1-xsru.onrender.compp.com/";
 if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
   BASE_URL = "https://full-stack-social-media-app-1-xsru.onrender.com/";
 }
