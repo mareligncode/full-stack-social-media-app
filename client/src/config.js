8ba00874f652cd95a9df771f351f802c4bf306a8
mareligncode <yimermarelign@gmail.com>
2025-11-26 22:21:49 +0300

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 let BASE_URL = "https://post-it-heroku.herokuapp.com/";
 if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
-  BASE_URL = "https://full-stack-social-media-app-three.vercel.app/";
+  BASE_URL = "https://full-stack-social-media-app-1-xsru.onrender.com/";
 }
 
 export { BASE_URL };
